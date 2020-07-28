158b7923c6d0ce14329916078040592940290723
OliD <Oliver.drechsler@gmx.de>
2020-07-29 00:33:02 +0200

    first version 0.0.1

---
+++ README.md
@@ -0,0 +1,108 @@
+[![GitHub Action Status](https://github.com/od2017/reply_dns_and_arp_with_wol/workflows/reply_dns_and_arp_with_wol/badge.svg)](https://github.com/od2017/reply_dns_and_arp_with_wol/workflows/reply_dns_and_arp_with_wol/badge.svg)
+[![wemake-python-styleguide](https://img.shields.io/badge/style-wemake-000000.svg)](https://github.com/wemake-services/wemake-python-styleguide)
+
+# MADwaS - (M)onitoring (A)RP queries and (D)NS queries to (w)akeup (a) (S)erver
+
+- [MADwaS - (M)onitoring (A)RP queries and (D)NS queries to (w)akeup (a) (S)erver](#madwas---monitoring-arp-queries-and-dns-queries-to-wakeup-a-server)
+  - [Intro](#intro)
+  - [UseCase](#usecase)
+  - [minimum requirements](#minimum-requirements)
+  - [first time setup](#first-time-setup)
+  - [install script as a systemd service](#install-script-as-a-systemd-service)
+  - [config.yaml parameters should be self explained](#configyaml-parameters-should-be-self-explained)
+
+
+## Intro
+
+This script sniffs on a network interface for:  
+
+- DNS Name query request  
+and / or
+- ARP (who has) IPAddress request  
+
+It checks if a destination ip (server) is alive via icmp.  
+If not it sends a wake on lan magic packet and waits for a defined time period for the next check.  
+If a wakeup paket is send, it sends via local available mail service a notification mail about the wake up event.  
+
+## UseCase
+
+I ran at home a [VDR](http://tvdr.de/) home theater pc.  
+This computer also acts as my file server (samba).  
+To save power consumption, this PC is often powered of.  
+It powers it self on for recording TV shows and powers off it self.  
+For that i use some plugins like [EPGSearch](http://www.vdr-wiki.de/wiki/index.php/Epgsearch-plugin) [ACPIWakeup](http://www.vdr-wiki.de/wiki/index.php/ACPI_Wakeup)
+So, mostly when i want to access my files the VDR is often powered off and my  
+File Server is not accessable.  
+To power on my VDR via LAN, i use the [wake on lan](http://www.vdr-wiki.de/wiki/index.php/WAKE_ON_LAN) feature.  
+But there's no independend place who does it automatically for my.  
+Okay, you can maybe run a wake on lan script on your client PC.  
+But i want automaticylly power on my VDR File Server when i come at home.  
+I use the [PhotoSync](https://www.photosync-app.com/de/index.html) App on my smartphone  
+to automatically tranfer all new photos to the VDR Server.  
+This is done via GeoFencing and when my smartphone connects into my WLAN.  
+So, i must nothing do, magic things happens and i have every time my fotos backuped.  
+
+For that use case i need a independend third instance.  
+I have a Raspberry Pi running in my LAN which permanently powered on.  
+[PIHOLE](https://pi-hole.net/) runs on this one, but this does not play a role for my program/script.  
+It just has to run permanently somewhere.  
+
+## minimum requirements
+
+- sudo / root permissions to run dns_wol.py script
+- min. Python 3.6 
+- Python pip installed
+- sendmail or other mail daemon running on host to send a mail i case of a wake up event occured  
+- LAN interface must be in the same LAN/VLAN or WLAN as File Server
+
+LAN Device dependend capability:
+
+- connected to a LAN Switch Device - only ARP (who has) queries will work
+- connected to a LAN HUB Device - DNS queries will work
+- connected to a LAN Switch Device - but script runs on a DNS Server or [PiHole](https://pi-hole.net/), DNS queries will also work
+- connected to WLAN - WLAN Router must allow communication between WLAN Device - otherwise nothing will work.
+
+Host:
+
+- must be every time powered on
+
+
+## first time setup
+
+clone repo and configure settings
+```bash
+git clone [<repo url>](https://github.com/od2017/MADwaS.git)
+cd MADwaS
+cp dns_wol.py /usr/local/bin
+cp config_template.yaml /usr/bin/local/config.yaml
+cp log_config /usr/bin/local
+vi /usr/local/bin/config.yaml  # modify config.yaml to your needs
+```
+
+install python dependencies
+```bash
+pip install -r requirements
+python3 dns_wol.py  # run first time test and watch console output
+``` 
+
+## install script as a systemd service
+
+The file `dns_wol.service` is a example file for systemd and must be adjusted to  
+your personal needs. Copy the file to `/etc/systemd/system`.  
+And run bash `systemctl enable dns_wol.service` command to enable this new service.  
+To start service run `systemctl start dns_wol.service`  
+
+## config.yaml parameters should be self explained
+
+Customize in the config.yaml with following vars:  
+own_ip: "<host ip where script runs on>"  
+own_mac: "<mac address where script runs on>"   
+  
+listening_name:  
+  - "list of <dnsname query for which should be listened>"  
+  - "seconde name entry"  
+listening_ip: "<ip address for which should be listened>"  
+listening_mac: "<mac address for which the arp request should be listened>"  
+  
+from_mail: "<local sendmail daemon from mail address>"  
+to_mail: "<local sendmail daemon to mail address>"  
---
+++ dns_wol.py
@@ -0,0 +1,430 @@
+#!/usr/bin/env python3
+
+import sys
+from scapy.all import (
+    ARP,
+    IP,
+    ICMP,
+    DNS,
+    sniff,
+    sr1,
+    conf,
+)
+from scapy.layers.all import log
+from wakeonlan import send_magic_packet
+import time
+import os
+import socket
+import yaml
+import smtplib
+from email.parser import Parser
+import threading
+import queue
+import logging
+import logging.config
+
+
+# static library variables
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+conf.sniff_promisc = True
+
+
+class Wakeup_Thread:
+    """
+    Stores attributes for monitored host to wakeup
+
+    Attributes:
+    searched_ip: monitored/listend ip / to wakeup
+    :type searched_ip: str
+    searched_mac: monitored/listend mac address / to wakeup
+    :type searched_mac: str
+    request_type: type of request - ARP (who is) or DNS name query
+    :type request_type: str
+    src_ip: orginating / source ip who has send this request
+    :type src_ip: str
+    searched_dns: queried dns name
+    :type searched_dns: default == None or provided str
+    """
+
+    def __init__(self, searched_ip, searched_mac, request_type, src_ip, 
+                 searched_dns=None):
+        self.searched_ip = searched_ip
+        self.searched_mac = searched_mac
+        self.request_type = request_type
+        self.src_ip = src_ip
+        self.searched_dns = searched_dns
+
+
+class Configuration:
+    """Reads gerneral yaml Config file into class.
+
+    Attributes:
+        own_ip: IP Address of host system where this code runs on
+        :type own_ip: str
+        own_mac: MAC Address of host system where this code runs on
+        :type own_mac: str
+        listing_ip: list of ip address which will be monitored / watched for action
+        :type listing_ip: list with str
+        listing_mac: list of mac address which will be monitored / watched for action
+        :type listing_mac: list with str
+        listing_name: list of dns name which will be monitored / watched for action
+        :type listing_name: list with str
+        from_mail: from mail address
+        :type from_mail: str
+        to_mail: to send mail address
+        :type to_mail: str
+    """
+
+    def __init__(self):
+        """Initial class definition."""
+        self.define_config_file()
+        self.read_config(self.config_file)
+        self.own_ip = self.config["own_ip"]
+        self.own_mac = self.config["own_mac"]
+        self.listening_ip = self.config["listening_ip"]
+        self.listening_name = self.config["listening_name"]
+        self.listening_mac = self.config["listening_mac"]
+        self.from_mail = self.config["from_mail"]
+        self.to_mail = self.config["to_mail"]
+
+    def read_config(self, config_file):
+        """
+        Reads config.yaml file into variables.
+
+        :param none
+        :return: config: variable
+        :rtype: dict
+        """
+        with open(file=config_file, mode="r") as file:
+            self.config = yaml.load(file, Loader=yaml.SafeLoader)
+        logger.debug(self.config)
+
+    def define_config_file(self):
+        """
+        Checks and defines Config yaml path.
+
+        Defines a new class path atrribute.
+        """
+        if os.path.isfile(os.path.dirname(os.path.abspath(__file__)) + "/config.yaml"):
+            self.config_file = os.path.dirname(os.path.abspath(__file__)) + "/config.yaml"
+            logger.debug(self.config_file)
+        else:
+            print("else")
+            self.config_file = (
+                os.path.dirname(os.path.abspath(__file__)) + "/config_template.yaml"
+            )
+            logger.debug(self.config_file)
+
+
+# def syslog_logging(message):
+#     """
+#     Logs message to standard syslog.
+
+#     :param message: message which should be logged
+#     :type message: string
+#     :return: none
+#     :rtype: None
+#     """
+#     syslog.syslog(syslog.LOG_INFO, message)
+
+
+def sendmail(_text, _from, _to):
+    """
+    Send a message to via local message queue.
+
+    :param _text: message text which will send
+    :type _text: str
+    :param _from: from mail address
+    :type _from: str
+    :param _to: to mail address
+    :type _to: str
+    :return: True/False, Tuple with smtp status code, message str
+    :rtype: boolean, tuple(int(status_code),str(mesasge of code))
+    """
+    _message = Parser().parsestr(f"From: <{_from}>\n"
+                                 f"To: <{_to}>\n"
+                                 f"Subject: {_text}\n"
+                                 "\n"
+                                 f"{_text}\n")
+    s = smtplib.SMTP('localhost')
+    s.send_message(_message)
+    result = s.quit()
+    logger.debug(f"sendmail result code {result}")
+    if result[0] is int(221):
+        logger.debug(f"return True, {result}")
+        return True, result
+    else:
+        logger.debug(f"return False, {result}")
+        return False, result
+
+def icmp_check(ipaddress):
+    """
+    Send a ping / icmp to ip to check if it is online.
+
+    :param ipaddress: ip v4 address
+    :type ipaddress: str
+    :return: success / failed boolean
+    :rtype: boolean
+    """
+    logger.info("ICMP checking if {0} is alive".format(ipaddress))
+    ans1= sr1(IP(dst=ipaddress) / ICMP(), timeout=1, verbose=0)
+    logger.debug(f"first responce sr1={ans1}")
+    time.sleep(0.5)
+    resp = sr1(IP(dst=ipaddress) / ICMP(), timeout=1, verbose=0)
+    logger.debug(f"second responce sr1={resp}")
+    if resp is None:
+        logger.debug("icmp check responce = None")
+        logger.info("ICMP to IP {} is not answering".format(ipaddress))
+        return False
+    else:
+        logger.debug("icmp check responce = {}".format(resp.summary()))
+        logger.info("IP {} is alive!".format(ipaddress))
+        return True
+
+
+def wakeup_monitored_host(wakeup_class):
+    """
+    Wake monitored host / ip, if not alive via icmp
+
+    :param wakeup_class: from scapy sniffed thread new created class Wakeup_Thread for wakeup host
+    :type _object_class_form_queue: class 
+    :return: if wakeup performed = True / if host alive = False
+    :rtype: boolean
+    """
+    try:
+        if wakeup_class.searched_dns is None:
+            _asked_for = wakeup_class.searched_ip
+        else:
+            _asked_for = wakeup_class.searched_dns
+
+        logger.info(f"{wakeup_class.request_type} request detected - IP {wakeup_class.src_ip} asks for {_asked_for}")
+
+        icmp_result = icmp_check(wakeup_class.searched_ip)
+        logger.debug(f"icmp_result: {icmp_result}")
+
+        if icmp_result is False:
+
+            get_hostname_resolution = str(get_hostname(wakeup_class.src_ip))
+            logger.debug(f"hostname resolution = {get_hostname_resolution}")
+            logger.info("WoL WakeUp - {0} Request detected - IP {1} / {2} asks for {3}".format(
+                wakeup_class.request_type, wakeup_class.src_ip,
+                get_hostname_resolution,
+                _asked_for))
+
+            logger.debug(f"send wol paket to {wakeup_class.searched_ip}")
+            send_magic_packet(wakeup_class.searched_mac)
+            logger.debug("send now email about wakeup")
+            sendmail("WoL WakeUp - {0} Request detected - IP {1} / {2} asks for {3}".format(
+                wakeup_class.request_type, wakeup_class.src_ip,
+                get_hostname_resolution,
+                _asked_for),
+                config.from_mail,
+                config.to_mail)
+
+            logger.debug(f"now wait to spin up host for {config.wait_time} seconds")
+            time.sleep(config.wait_time)
+
+            logger.debug("Method return True")
+            return True
+
+        else:
+            logger.info(f"no wake - icmp check: host is alive!")
+            logger.debug("Method return False")
+            return False
+    except Exception as e:
+        logger.exception("Exception occurred", exc_info=True)
+        pass
+    logger.debug("return False")
+    return False
+
+
+
+def arp_check(pkt):
+    """Check ethernet packet for arp request
+
+    Checks if an arp request is send for watched/monitored MAC address and
+    sends than a WOL magic packet if IP is not alive/UP
+
+    :param pkt: sniffed full ethernet packet
+    :type pkt: class
+    :return: give a boolean feedback if a wol was send or not
+    :rtype: boolean
+    """
+    logger.debug(f"check arp paket {pkt[ARP].op} == 1")
+    if pkt[ARP].op == 1:
+        searched_arp_ip = pkt[ARP].pdst
+        requestor_arp_ip = pkt[ARP].psrc
+        arp_asking_mac = pkt[ARP].hwsrc
+        logger.debug(f"hwsrc {arp_asking_mac}; psrc {requestor_arp_ip}; pdst {searched_arp_ip}")
+        if searched_arp_ip == config.listening_ip:
+            logger.debug(f"{searched_arp_ip} == {config.listening_ip}")
+            if requestor_arp_ip != config.own_ip:
+                logger.debug(f"{requestor_arp_ip} != {config.own_ip}")
+                wakeup_objects = Wakeup_Thread(
+                    searched_ip=config.listening_ip,
+                    searched_mac=config.listening_mac,
+                    request_type="ARP",
+                    src_ip=requestor_arp_ip,
+                    searched_dns=None
+                    )
+                add_object_to_thread_queue(wakeup_objects)
+                return True
+
+    logger.debug("return False")
+    # maybe to fix below for unwanted syslog output 
+    return False
+
+
+def dns_query_check(pkt):
+    """Checks ethernet packet for dns query.
+
+    Checks if an IP dns query is send/received for watched/monitored IP address and
+    sends than a WOL magic packet if IP is not alive/UP
+
+    :param pkt: sniffed full ethernet packet
+    :type pkt: class
+    :return: give a boolean feedback if a wol was send or not
+    :rtype: boolean
+    """
+    try:
+        logger.debug(f"check dns query {str(pkt.getlayer(DNS).qd.qname.decode('ASCII')).lower().rstrip('.')}")
+
+        if str(pkt.getlayer(DNS).qd.qname.decode("ASCII")).lower().rstrip('.') in str(config.listening_name).lower():
+            ip_src = pkt[IP].src
+            dns_name = str(pkt.getlayer(DNS).qd.qname.decode("ASCII")).lower().rstrip('.')
+            logger.info(f"{str(pkt.getlayer(DNS).qd.qname.decode('ASCII')).lower().rstrip('.')} is in {str(config.listening_name).lower()}")
+            wakeup_objects = Wakeup_Thread(
+                searched_ip=config.listening_ip,
+                searched_mac=config.listening_mac,
+                request_type="DNS Query",
+                src_ip=ip_src,
+                searched_dns=dns_name
+                )
+            add_object_to_thread_queue(wakeup_objects)
+            return True
+    except Exception as e:
+        logger.exception("Exception occurred", exc_info=True)
+        pass
+    logger.debug("return False")
+    return False
+
+
+def sniff_arp_and_dns(pkt):
+    """Precheck sniffed ethernet packet for arp or dns query.
+
+    Sniffed ethernet packet will be further checked,
+    if it is a IP packet with DNS query or
+    if it is a ARP packet.
+
+    :param pkt: sniffed full ethernet packet
+    :type pkt: class
+    :return: give result of sub method back
+    :rtype: result: boolean
+    """
+    result = None
+    logger.debug(f"sniffed paket to check: {pkt.summary()}")
+    # show full paket to stdout
+    # logger.debug(pkt.show())
+    if ARP in pkt:
+        logger.debug('ARP paket detected')
+        result = arp_check(pkt)
+
+    if pkt.haslayer(DNS) and pkt.getlayer(DNS).qr == 0:
+        logger.debug('DNS paket detected')
+        result = dns_query_check(pkt)
+    logger.debug(f"return result {result}")
+    return result
+
+
+def get_hostname(ip):
+    """Get hostname of IP.
+
+    This method returns the 'True Host' name for a
+    given IP address.
+    It uses a socks gethostname query.
+
+    :param ip: ipv4 address
+    :type ip: str
+    :return: hostname
+    :rtype: string
+    """
+    try:
+        data = socket.gethostbyaddr(ip)
+        # host = repr(data[0])
+        # host = repr(data)
+        host = data
+        logger.debug(f'resolved hostname: {host[0]}')
+        return host[0]
+    except Exception:
+        logger.exception("Exception occurred", exc_info=True)
+        logger.debug("execption ip to hostname not resolved")
+        return False
+
+
+def load_config():
+    """Load config file.
+
+    Loads config file into a class object which gloablly available variable.
+
+    :return: global config class variable
+    :rtype: class
+    """
+    global config
+    config = Configuration()
+
+
+def check_thread_queue():
+
+    logger.info("starting endless check queue loop")
+
+    while True:
+        if not workQueue.empty():
+            logger.debug("get queue paket")
+            queueLock.acquire()
+            _object_class = workQueue.get_nowait()
+            wakeup_monitored_host(_object_class)
+            queueLock.release()
+        else:
+            logger.debug("empty queue")
+
+        time.sleep(0.01)
+
+
+def add_object_to_thread_queue(_object_class):
+
+    queueLock.acquire()
+    logger.debug("add new class to queue for wakeup thread")
+    workQueue.put(_object_class)
+    queueLock.release()
+
+
+if __name__ == "__main__":
+    """Main method"""
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/log_config.yaml', 'r') as f:
+        log_config = yaml.safe_load(f.read())
+        logging.config.dictConfig(log_config)
+    logger = logging.getLogger(__name__)
+
+    logger.info("reading config file")
+    load_config()
+
+    logger.info("setup thread queue")
+    queueLock = threading.Lock()
+    workQueue = queue.Queue()
+
+    loop_queue_check = threading.Thread(target=check_thread_queue)
+    logger.info("before running endless - queue check thread")
+    loop_queue_check.start()
+
+    try:
+        logger.info("starting scapy sniffing pakets")
+        sniff(prn=sniff_arp_and_dns, filter="arp[6:2] == 1 or udp dst port 53", store=0)
+        # sniff(prn=add_paket_to_thread_queue, lfilter=lambda a: a[ARP].opcode == 1, filter="udp dst port 53", store=0)
+    except KeyboardInterrupt:
+        logger.exception("Exception occurred", exc_info=True)
+        logger.info("User Requested Shutdown...")
+        logger.info("Exiting...")
+        sys.exit(1)
+
+    logger.info("wait for the thread to finish")
+    logger.info("all done")
