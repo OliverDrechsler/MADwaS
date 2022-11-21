23aae8c14825775d09f1e6c7a47e986834a30bb6
Oliver Drechsler <@users.noreply.github.com>
2022-11-21 23:35:32 +0100

    multiple_ips_list: savepoint:

--- dns_wol.py
+++ dns_wol.py
@@ -260,7 +260,8 @@ def arp_check(pkt):
         logger.debug(
             f"hwsrc {arp_asking_mac}; psrc {requestor_arp_ip}; pdst {searched_arp_ip}"
         )
-        monitored_dict = [ listingDict for listingDict in config.monitoring if listingDict.get('ip') == searched_arp_ip ]
+        key, value = "ip", searched_arp_ip
+        monitored_dict = [ listingDict for listingDict in config.monitoring if listingDict.get(key) == value ]
         logger.debug(f"monitored {monitored_dict['ip']} == searched {searched_arp_ip}")
         if not bool(monitored_dict):
             if requestor_arp_ip not in config.blocked_ip or requestor_arp_ip != monitored_dict['ip']:
@@ -295,8 +296,8 @@ def dns_query_check(pkt):
         logger.debug(
             f"check dns query {str(pkt.getlayer(DNS).qd.qname.decode('ASCII')).lower().rstrip('.')}"
         )
-        monitored_dict = [ listingDict for listingDict in config.monitoring if listingDict.get('dns_name') == str(
-            pkt.getlayer(DNS).qd.qname.decode("ASCII")).lower().rstrip(".") ]
+        key,value = 'dns_name',(pkt.getlayer(DNS).qd.qname.decode("ASCII")).lower().rstrip(".")
+        monitored_dict = [ listingDict for listingDict in config.monitoring if listingDict.get(key) == value ]
         if not bool(monitored_dict):
             logger.info(f"monitored {monitored_dict['dns_name']} == searched " +
                 f"{str(pkt.getlayer(DNS).qd.qname.decode('ASCII')).lower().rstrip('.')}")
